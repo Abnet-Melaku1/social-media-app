0a268802e508c28d78bb11cded322a874960e22e
Abnet Melaku <abnetmelaku20@gmail.com>
2023-07-20 11:06:28 -0700

    SideBar,sign up, sign in UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import * as ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { store } from "./app/store"
+import App from "./App"
+import React from "react"
+const colors = {
+  brand: {
+    900: "#3f2f80",
+    800: "#4a32a1",
+    700: "#5b3ac8",
+    600: "#6949e2",
+    500: "#7a68ef",
+    400: "#8a83f6",
+    300: "#a6a8fb",
+    200: "#c8cbfd",
+  },
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = extendTheme({ colors })
+
+const rootElement = document.getElementById("root")
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
 )
