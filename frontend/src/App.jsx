db723a297de6bed9ad3ac4f4b2159a564208c8ff
Abnet Melaku <abnetmelaku20@gmail.com>
2023-07-22 05:40:25 -0700

    Sidebar, sign up, sign in Removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import SignupCard from "./pages/SignUp"
-import SidebarWithHeader from "./components/Nav"
 
 function App() {
   return (
     <>
       <Router>
-        <div>
-          <SidebarWithHeader />
-          <Routes></Routes>
-        </div>
+        <Routes></Routes>
       </Router>
     </>
   )
