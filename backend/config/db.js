ca6b814d19309289b42558f4862854010e9516ef
Abnet Melaku <abnetmelaku20@gmail.com>
2023-07-25 03:50:31 -0700

    auth route fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const connetDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI).then(() => {
-      console.log("The database is connected successfully.");
-    });
+      console.log("The database is connected successfully.")
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
-module.exports = connetDB;
+}
+module.exports = connetDB
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const dotenv = require("dotenv").config();
-const user = require("./routes/authRoutes");
-const connectDb = require("./config/db");
-// const users = require("./routes/userRoutes");
-const { errorHandler } = require("./middlewares/errorMiddleware");
-const port = process.env.PORT || 5000;
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const dotenv = require("dotenv").config()
 
-const app = express();
-app.use(express.json());
-app.use(morgan("common"));
-app.use("/api/users", user);
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/posts", require("./routes/postRoutes"));
-app.use(errorHandler);
+const connectDb = require("./config/db")
+// const users = require("./routes/userRoutes");
+const { errorHandler } = require("./middlewares/errorMiddleware")
+const port = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+app.use(morgan("common"))
+app.use("/api/auth", require("./routes/authRoutes"))
+app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/posts", require("./routes/postRoutes"))
+app.use(errorHandler)
 app.listen(port, () => {
-  console.log(`app started on server ${port}`);
-});
-connectDb();
+  console.log(`app started on server ${port}`)
+})
+connectDb()
