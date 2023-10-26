4934701007a3ecbb4fa0084da353e0c7364d52b7
Abnet Melaku <abnetmelaku20@gmail.com>
2023-10-27 00:46:16 +0300

    cors error fixed 4

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,25 @@ const { errorHandler } = require("./middlewares/errorMiddleware")
 const port = process.env.PORT || 5000
 
 const app = express()
-const allowedOrigins = ["https://pazzion-social-media.vercel.app/"]
 
+app.use(function (req, res, next) {
+  const allowedOrigins = [
+    "http://localhost:5173",
+    "https://pazzion-social-media.vercel.app/",
+    "https://pazzion-social-media.vercel.app/",
+  ]
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin)
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  res.header("Access-Control-Allow-credentials", true)
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE")
+  next()
+})
 // app.use((req, res, next) => {
 //   res.setHeader("Access-Control-Allow-Origin", "*")
 //   res.setHeader(
@@ -22,15 +39,15 @@ const allowedOrigins = ["https://pazzion-social-media.vercel.app/"]
 //   res.setHeader("Access-Control-Allow-Headers", "*")
 //   next()
 // })
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://pazzion-social-media.vercel.app"
-  )
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://pazzion-social-media.vercel.app"
+//   )
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+//   next()
+// })
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
