102328a8f4b7b64591fb689ea411b1b766498c74
Abnet Melaku <abnetmelaku20@gmail.com>
2023-10-27 00:38:45 +0300

    cors error fixed 3

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,15 @@ const port = process.env.PORT || 5000
 const app = express()
 const allowedOrigins = ["https://pazzion-social-media.vercel.app/"]
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  )
-  res.setHeader("Access-Control-Allow-Headers", "*")
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*")
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   )
+//   res.setHeader("Access-Control-Allow-Headers", "*")
+//   next()
+// })
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
